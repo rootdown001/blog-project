69019e8c0a5ffc63bff19af3b3829ca197bbf0ff
Lance Anderson <lance.a.585@gmail.com>
2023-10-12 20:31:02 -0700

    ADD: Add users.map in jsx of Users

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -5,5 +5,26 @@ export default function Users() {
 
   console.log("🚀 ~ file: Users.jsx:6 ~ Users ~ users:", users);
 
-  return <div>Users</div>;
+  return (
+    <div className="container">
+      <h1 className="page-title">Users</h1>
+      <div className="card-grid">
+        {users.map((user) => {
+          return (
+            <div key={user.id} className="card">
+              <div className="card-header">{user.name}</div>
+              <div className="card-body">
+                <div>{user.company.name}</div>
+                <div>{user.website}</div>
+                <div>{user.email}</div>
+              </div>
+              <div className="card-footer">
+                <a class="btn">View</a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
