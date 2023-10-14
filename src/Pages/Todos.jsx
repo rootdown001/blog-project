756ffee799a5649a8bf613326f7f6277a3d879e8
Lance Anderson <lance.a.585@gmail.com>
2023-10-13 18:06:46 -0700

    ADD: Add todos loader to router ADD: Add Todos page useLoaderDate, jsx, terery logic for className

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -1,3 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+
 export default function Todos() {
-  return <div>Todos</div>;
+  const todos = useLoaderData();
+  console.log("🚀 ~ file: Todos.jsx:5 ~ Todos ~ todos:", todos);
+
+  return (
+    <div className="container">
+      <h1 className="page-title">Todos</h1>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              className={todo.completed === true ? "strike-through" : ""}
+            >
+              {todo.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
