5595606bb4c771fe81bd3a2e9408d79508ef6682
Lance Anderson <lance.a.585@gmail.com>
2023-10-13 18:40:43 -0700

    ADD: Add Post pasge for singal posts UPDATE: Update router to use index: true in posts nesting

--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Posts from "./Pages/Posts";
 import Users from "./Pages/Users";
 import Todos from "./Pages/Todos";
 import Temp from "./Pages/Temp";
+import Post from "./Pages/Post";
 
 export const router = createBrowserRouter([
   {
@@ -12,10 +13,26 @@ export const router = createBrowserRouter([
       { path: "/", element: <Temp /> },
       {
         path: "/posts",
-        loader: ({ request: { signal } }) => {
-          return fetch("http://127.0.0.1:3000/posts", { signal });
-        },
-        element: <Posts />,
+        // loader: ({ request: { signal } }) => {
+        //   return fetch("http://127.0.0.1:3000/posts", { signal });
+        // },
+        // element: <Posts />,
+        children: [
+          {
+            index: true,
+            loader: ({ request: { signal } }) => {
+              return fetch("http://127.0.0.1:3000/posts", { signal });
+            },
+            element: <Posts />,
+          },
+          {
+            path: ":id",
+            loader: ({ params, request: { signal } }) => {
+              return fetch("http://127.0.0.1:3000/posts/:id", { signal });
+            },
+            element: <Post />,
+          },
+        ],
       },
       {
         path: "/users",
