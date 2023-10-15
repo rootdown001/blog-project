e5cb66df09888d2c51dc3821dcebe4d292026f70
Lance Anderson <lance.a.585@gmail.com>
2023-10-15 16:20:02 -0700

    ADD: Add URLS.TODOS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export const URLS = {
   USERS: "http://127.0.0.1:3000/users",
   POSTS: "http://127.0.0.1:3000/posts",
   COMMENTS: "http://127.0.0.1:3000/comments",
+  TODOS: "http://127.0.0.1:3000/todos",
 };
 
 function App() {
