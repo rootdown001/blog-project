184623e2bf8c99e505c6bc04931bd4f02056602d
Lance Anderson <lance.a.585@gmail.com>
2023-10-15 16:03:38 -0700

    ADD: Add useFetch to get posts for user

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,8 +1,17 @@
 import { useLoaderData } from "react-router-dom";
+import { useFetch } from "../useFetch";
+import { URLS } from "../App";
 
 export default function User() {
   const user = useLoaderData();
   console.log("🚀 ~ file: User.jsx:5 ~ User ~ user:", user);
+
+  const {
+    data: posts,
+    isError: isPostsError,
+    isLoading: isPostsLoading,
+  } = useFetch(`${URLS.POSTS}?userId=${user.id}`);
+  console.log("🚀 ~ file: User.jsx:14 ~ User ~ posts:", posts);
 
   return (
     <div class="container">
@@ -30,85 +39,6 @@ export default function User() {
               quia et suscipit suscipit recusandae consequuntur expedita et cum
               reprehenderit molestiae ut ut quas totam nostrum rerum est autem
               sunt rem eveniet architecto
-            </div>
-          </div>
-          <div class="card-footer">
-            <a class="btn" href="posts.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">qui est esse</div>
-          <div class="card-body">
-            <div class="card-preview-text">
-              est rerum tempore vitae sequi sint nihil reprehenderit dolor
-              beatae ea dolores neque fugiat blanditiis voluptate porro vel
-              nihil molestiae ut reiciendis qui aperiam non debitis possimus qui
-              neque nisi nulla
-            </div>
-          </div>
-          <div class="card-footer">
-            <a class="btn" href="posts.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            ea molestias quasi exercitationem repellat qui ipsa sit aut
-          </div>
-          <div class="card-body">
-            <div class="card-preview-text">
-              et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad
-              voluptatem doloribus vel accusantium quis pariatur molestiae porro
-              eius odio et labore et velit aut
-            </div>
-          </div>
-          <div class="card-footer">
-            <a class="btn" href="posts.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">eum et est occaecati</div>
-          <div class="card-body">
-            <div class="card-preview-text">
-              ullam et saepe reiciendis voluptatem adipisci sit amet autem
-              assumenda provident rerum culpa quis hic commodi nesciunt rem
-              tenetur doloremque ipsam iure quis sunt voluptatem rerum illo
-              velit
-            </div>
-          </div>
-          <div class="card-footer">
-            <a class="btn" href="posts.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">nesciunt quas odio</div>
-          <div class="card-body">
-            <div class="card-preview-text">
-              repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed
-              est voluptatem omnis possimus esse voluptatibus quis est aut
-              tenetur dolor neque
-            </div>
-          </div>
-          <div class="card-footer">
-            <a class="btn" href="posts.html">
-              View
-            </a>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">dolorem eum magni eos aperiam quia</div>
-          <div class="card-body">
-            <div class="card-preview-text">
-              ut aspernatur corporis harum nihil quis provident sequi mollitia
-              nobis aliquid molestiae perspiciatis et ea nemo ab reprehenderit
-              accusantium quas voluptate dolores velit et doloremque molestiae
             </div>
           </div>
           <div class="card-footer">
