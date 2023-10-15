274e86bb385ca5ef019dd97f4fdef9f63264e582
Lance Anderson <lance.a.585@gmail.com>
2023-10-14 19:40:05 -0700

    UPDATE: Update urls to correct API endpoints

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -7,18 +7,18 @@ export default function Post() {
   // console.log("🚀 ~ file: Post.jsx:5 ~ Post ~ post:", post);
 
   const {
-    data: users,
+    data: user,
     isLoading: isUsersLoading,
     isError: isUsersError,
-  } = useFetch(URLS.USERS);
-  // console.log("🚀 ~ file: Post.jsx:10 ~ Post ~ users:", users);
+  } = useFetch(`${URLS.USERS}/${post.userId}`);
+  console.log("🚀 ~ file: Post.jsx:10 ~ Post ~ users:", user);
 
   const {
     data: comments,
     isLoading: isCommentsLoading,
     isError: isCommentsError,
-  } = useFetch(URLS.COMMENTS);
-  // console.log("🚀 ~ file: Post.jsx:21 ~ Post ~ comments:", comments);
+  } = useFetch(`${URLS.POSTS}/${post.id}/comments`);
+  console.log("🚀 ~ file: Post.jsx:21 ~ Post ~ comments:", comments);
 
   if (isUsersLoading || isCommentsLoading) {
     // Handle loading state
@@ -34,27 +34,22 @@ export default function Post() {
     <div className="container">
       <h1 className="page-title">{post.title}</h1>
       <span className="page-subtitle">
-        By:{" "}
-        <a href="user.html">
-          {users && users.find((user) => user.id === post.userId)?.name}
-        </a>
+        By: <a href="user.html">{user?.name}</a>
       </span>
       <div>{post.body}</div>
       <h3 className="mt-4 mb-2">Comments</h3>
       <div className="card-stack">
         {comments &&
-          comments
-            .filter((comment) => comment.postId === post.id)
-            .map((comment) => {
-              return (
-                <div key={comment.id} className="card">
-                  <div className="card-body">
-                    <div className="text-sm mb-1">{comment.email}</div>
-                    {comment.body}
-                  </div>
+          comments.map((comment) => {
+            return (
+              <div key={comment.id} className="card">
+                <div className="card-body">
+                  <div className="text-sm mb-1">{comment.email}</div>
+                  {comment.body}
                 </div>
-              );
-            })}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
