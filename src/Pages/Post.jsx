95e78b49239c9e3875b203817a43454dba6ae51a
Lance Anderson <lance.a.585@gmail.com>
2023-10-14 18:23:05 -0700

    UPDATE: Update Post to use useFetch for APIs

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,63 +1,60 @@
 import { useLoaderData } from "react-router-dom";
+import { useFetch } from "../useFetch";
+import { URLS } from "../App";
 
 export default function Post() {
   const post = useLoaderData();
+  // console.log("🚀 ~ file: Post.jsx:5 ~ Post ~ post:", post);
+
+  const {
+    data: users,
+    isLoading: isUsersLoading,
+    isError: isUsersError,
+  } = useFetch(URLS.USERS);
+  // console.log("🚀 ~ file: Post.jsx:10 ~ Post ~ users:", users);
+
+  const {
+    data: comments,
+    isLoading: isCommentsLoading,
+    isError: isCommentsError,
+  } = useFetch(URLS.COMMENTS);
+  // console.log("🚀 ~ file: Post.jsx:21 ~ Post ~ comments:", comments);
+
+  if (isUsersLoading || isCommentsLoading) {
+    // Handle loading state
+    return <div>Loading...</div>;
+  }
+
+  if (isUsersError || isCommentsError) {
+    // Handle error state
+    return <div>Error occurred while fetching data.</div>;
+  }
+
   return (
     <div className="container">
-      <h1 className="page-title">
-        sunt aut facere repellat provident occaecati excepturi optio
-        reprehenderit
-      </h1>
+      <h1 className="page-title">{post.title}</h1>
       <span className="page-subtitle">
-        By: <a href="user.html">Leanne Graham</a>
+        By:{" "}
+        <a href="user.html">
+          {users && users.find((user) => user.id === post.userId)?.name}
+        </a>
       </span>
-      <div>
-        quia et suscipit suscipit recusandae consequuntur expedita et cum
-        reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt
-        rem eveniet architecto
-      </div>
+      <div>{post.body}</div>
       <h3 className="mt-4 mb-2">Comments</h3>
       <div className="card-stack">
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Eliseo@gardner.biz</div>
-            laudantium enim quasi est quidem magnam voluptate ipsam eos tempora
-            quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente
-            accusantium
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Jayne_Kuhic@sydney.com</div>
-            est natus enim nihil est dolore omnis voluptatem numquam et omnis
-            occaecati quod ullam at voluptatem error expedita pariatur nihil
-            sint nostrum voluptatem reiciendis et
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Nikita@garfield.biz</div>
-            quia molestiae reprehenderit quasi aspernatur aut expedita occaecati
-            aliquam eveniet laudantium omnis quibusdam delectus saepe quia
-            accusamus maiores nam est cum et ducimus et vero voluptates
-            excepturi deleniti ratione
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Lew@alysha.tv</div>
-            non et atque occaecati deserunt quas accusantium unde odit nobis qui
-            voluptatem quia voluptas consequuntur itaque dolor et qui rerum
-            deleniti ut occaecati
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <div className="text-sm mb-1">Hayden@althea.biz</div>
-            harum non quasi et ratione tempore iure ex voluptates in ratione
-            harum architecto fugit inventore cupiditate voluptates magni quo et
-          </div>
-        </div>
+        {comments &&
+          comments
+            .filter((comment) => comment.postId === post.id)
+            .map((comment) => {
+              return (
+                <div key={comment.id} className="card">
+                  <div className="card-body">
+                    <div className="text-sm mb-1">{comment.email}</div>
+                    {comment.body}
+                  </div>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
