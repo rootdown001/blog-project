b996c4f0c15cd037b9783417241f5ed5910d5a75
Lance Anderson <lance.a.585@gmail.com>
2023-10-14 18:20:39 -0700

    ADD: Added useFetch

---
+++ src/useFetch.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(myUrl, options = {}) {
+  const [data, setData] = useState();
+  const [isError, setIsError] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(undefined);
+
+  useEffect(() => {
+    setData(undefined);
+    setIsError(undefined);
+    setIsLoading(true);
+
+    const controller = new AbortController();
+
+    fetch(myUrl, options, { signal: controller.signal })
+      .then((res) => {
+        if (res.status === 200 || res.status === 201) {
+          return res.json();
+        } else {
+          //   console.log("res: ", res);
+          return Promise.reject(res);
+        }
+      })
+      .then((d) => {
+        // console.log("d: ", d);
+        setData(d);
+      })
+      .catch((e) => {
+        if (e?.name === "AbortError") return;
+        setIsError(e);
+      })
+      .finally(() => {
+        if (controller.signal.aborted) return;
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [myUrl]);
+
+  return { data, isError, isLoading };
+}
