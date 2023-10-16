2869ebfc690314f07443605e534b458e7e6266c1
Lance Anderson <lance.a.585@gmail.com>
2023-10-15 20:30:26 -0700

    ADD: Add spinner

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,32 +1,36 @@
-import { useLoaderData, NavLink } from "react-router-dom";
+import { useLoaderData, NavLink, useNavigation } from "react-router-dom";
 
 export default function Users() {
   const users = useLoaderData();
-
   console.log("🚀 ~ file: Users.jsx:6 ~ Users ~ users:", users);
 
+  const { state } = useNavigation();
+
   return (
-    <div className="container">
-      <h1 className="page-title">Users</h1>
-      <div className="card-grid">
-        {users.map((user) => {
-          return (
-            <div key={user.id} className="card">
-              <div className="card-header">{user.name}</div>
-              <div className="card-body">
-                <div>{user.company.name}</div>
-                <div>{user.website}</div>
-                <div>{user.email}</div>
+    <>
+      <div className={state === "loading" ? "loading-spinner" : ""}></div>
+      <div className={state === "loading" ? "container loading" : "container"}>
+        <h1 className="page-title">Users</h1>
+        <div className="card-grid">
+          {users.map((user) => {
+            return (
+              <div key={user.id} className="card">
+                <div className="card-header">{user.name}</div>
+                <div className="card-body">
+                  <div>{user.company.name}</div>
+                  <div>{user.website}</div>
+                  <div>{user.email}</div>
+                </div>
+                <div className="card-footer">
+                  <NavLink to={`/users/${user.id}`} class="btn">
+                    View
+                  </NavLink>
+                </div>
               </div>
-              <div className="card-footer">
-                <NavLink to={`/users/${user.id}`} class="btn">
-                  View
-                </NavLink>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
