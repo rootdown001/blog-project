26403eb9cc3aade5a603a4a29968068fd212768c
Lance Anderson <lance.a.585@gmail.com>
2023-10-15 20:26:00 -0700

    ADD: Add spinning loader

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -1,24 +1,29 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 
 export default function Todos() {
   const todos = useLoaderData();
   console.log("🚀 ~ file: Todos.jsx:5 ~ Todos ~ todos:", todos);
 
+  const { state } = useNavigation();
+
   return (
-    <div className="container">
-      <h1 className="page-title">Todos</h1>
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li
-              key={todo.id}
-              className={todo.completed ? "strike-through" : ""}
-            >
-              {todo.title}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div className={state === "loading" ? "loading-spinner" : ""}></div>
+      <div className={state === "loading" ? "container loading" : ""}>
+        <h1 className="page-title">Todos</h1>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li
+                key={todo.id}
+                className={todo.completed ? "strike-through" : ""}
+              >
+                {todo.title}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
