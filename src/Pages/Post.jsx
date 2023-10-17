f3900e0b98afacaad5214e951301e3d5ddd7fcdc
Lance Anderson <lance.a.585@gmail.com>
2023-10-16 18:54:45 -0700

    ADD: Add spinner & blur

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,10 +1,12 @@
-import { useLoaderData, NavLink } from "react-router-dom";
+import { useLoaderData, NavLink, useNavigation } from "react-router-dom";
 import { useFetch } from "../useFetch";
 import { URLS } from "../App";
 
 export default function Post() {
   const post = useLoaderData();
   // console.log("🚀 ~ file: Post.jsx:5 ~ Post ~ post:", post);
+
+  const { state } = useNavigation();
 
   const {
     data: user,
@@ -20,37 +22,47 @@ export default function Post() {
   } = useFetch(`${URLS.POSTS}/${post.id}/comments`);
   console.log("🚀 ~ file: Post.jsx:21 ~ Post ~ comments:", comments);
 
-  if (isUsersLoading || isCommentsLoading) {
-    // Handle loading state
-    return <div>Loading...</div>;
-  }
-
   if (isUsersError || isCommentsError) {
     // Handle error state
     return <div>Error occurred while fetching data.</div>;
   }
 
   return (
-    <div className="container">
-      <h1 className="page-title">{post.title}</h1>
-      <span className="page-subtitle">
-        By: <NavLink to={`/users/${user?.id}`}>{user?.name}</NavLink>
-      </span>
-      <div>{post.body}</div>
-      <h3 className="mt-4 mb-2">Comments</h3>
-      <div className="card-stack">
-        {comments &&
-          comments.map((comment) => {
-            return (
-              <div key={comment.id} className="card">
-                <div className="card-body">
-                  <div className="text-sm mb-1">{comment.email}</div>
-                  {comment.body}
+    <>
+      <div
+        className={
+          state === "loading" || isUsersLoading || isCommentsLoading
+            ? "loading-spinner"
+            : ""
+        }
+      ></div>
+      <div
+        className={
+          state === "loading" || isUsersLoading || isCommentsLoading
+            ? "container loading"
+            : "container"
+        }
+      >
+        <h1 className="page-title">{post.title}</h1>
+        <span className="page-subtitle">
+          By: <NavLink to={`/users/${user?.id}`}>{user?.name}</NavLink>
+        </span>
+        <div>{post.body}</div>
+        <h3 className="mt-4 mb-2">Comments</h3>
+        <div className="card-stack">
+          {comments &&
+            comments.map((comment) => {
+              return (
+                <div key={comment.id} className="card">
+                  <div className="card-body">
+                    <div className="text-sm mb-1">{comment.email}</div>
+                    {comment.body}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
