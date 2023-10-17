ac715a0b93478433a6a63091046d9b5448f5e4df
Lance Anderson <lance.a.585@gmail.com>
2023-10-16 20:37:46 -0700

    REMOVE: Remove comments

--- src/router.jsx
+++ src/router.jsx
@@ -16,10 +16,6 @@ export const router = createBrowserRouter([
       { path: "/", element: <Temp /> },
       {
         path: "/posts",
-        // loader: ({ request: { signal } }) => {
-        //   return fetch("http://127.0.0.1:3000/posts", { signal });
-        // },
-        // element: <Posts />,
         children: [
           {
             index: true,
