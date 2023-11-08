2be360e530778956ddc6b7aa2b87748a1bfe32d0
Lance Anderson <lance.a.585@gmail.com>
2023-11-07 20:05:32 -0800

    UPDATE: Change baseUrl to not use .env - debug why

--- src/api/base.js
+++ src/api/base.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const baseApi = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://localhost:3000",
 });
+
+// TODO: debug why .env doesn't work to make baseURL
