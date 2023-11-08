8041bc2ad34fd3fd39aabec04b92483d1ebabc61
Lance Anderson <lance.a.585@gmail.com>
2023-11-07 17:10:33 -0800

    ADD: Add coments

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -4,7 +4,9 @@ import Navbar from "../Navbar";
 export default function NavLayout() {
   return (
     <>
+      {/* call Navbar */}
       <Navbar />
+      {/* div with "container" arount Outlet for spinner */}
       <div className={"container"}>
         <Outlet />
       </div>
