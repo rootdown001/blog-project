a614c7909711f5b61b0aaf6762523532f6617b58
Lance Anderson <lance.a.585@gmail.com>
2023-11-07 16:40:41 -0800

    UPDATE: Update router so path: "/" is higher

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
