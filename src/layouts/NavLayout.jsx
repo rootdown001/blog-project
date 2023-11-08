594182417b67b69bca75e4214ae78b3aaccc21c7
Lance Anderson <lance.a.585@gmail.com>
2023-11-07 18:41:50 -0800

    Add: Add state and Navigation Add: Add div for spinner

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -1,7 +1,15 @@
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import {
+  Outlet,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+} from "react-router-dom";
 import Navbar from "../Navbar";
 
 export default function NavLayout() {
+  // get state from useLocation
+  const { state } = useNavigation();
+
   return (
     <>
       {/* call Navbar */}
@@ -9,7 +17,8 @@ export default function NavLayout() {
       {/* ad ScrollRestoration to adjust scroll to top when changing pages */}
       <ScrollRestoration />
       {/* div with "container" arount Outlet for spinner */}
-      <div className={"container"}>
+      <div className={state === "loading" ? "loading-spinner" : ""}></div>
+      <div className={state === "loading" ? "container loading" : "container"}>
         <Outlet />
       </div>
     </>
