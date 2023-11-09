dc88629157bba63fc0314bf1515580722baff4ec
Lance Anderson <lance.a.585@gmail.com>
2023-11-08 18:09:53 -0800

    UPDATE: Move env.development to root ## Why
    - To make it easier to use `env.development` in other repos

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const baseApi = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
-// TODO: debug why .env doesn't work to make baseURL
