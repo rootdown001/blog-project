6387c961c201efd6a0fb742abc4856618f4f4ffa
Lance Anderson <lance.a.585@gmail.com>
2023-11-08 19:52:33 -0800

    UPDATE: update Error page to useRouteError

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,12 +1,12 @@
+import { useRouteError } from "react-router-dom";
 import Navbar from "../Navbar";
 
 export default function Error() {
+  const error = useRouteError();
+
   return (
     <>
-      <Navbar />
-      <div className="container">
-        <div className="page-title error">404 Error - Page Not Found...</div>
-      </div>
+      <div>error - something is wrong</div>
     </>
   );
 }
