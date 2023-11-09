6fa6f7eb84bff11fae2f18eae2fd3a87e801fee6
Lance Anderson <lance.a.585@gmail.com>
2023-11-08 20:03:57 -0800

    UPDATE: Add error.message & error.stack to ErrorPage

--- src/router.jsx
+++ src/router.jsx
@@ -64,7 +64,13 @@ export default function ErrorPage() {
 
   return (
     <>
-      <div>error - something is wrong</div>
+      <h1>Error - Something went wrong</h1>
+      {import.meta.env.MODE !== "production" && (
+        <>
+          <pre>{error.message}</pre>
+          <pre>{error.stack}</pre>
+        </>
+      )}
     </>
   );
 }
