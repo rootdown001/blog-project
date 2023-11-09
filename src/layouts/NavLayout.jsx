ba20950b8ab4a99c494c07a991331f8bfb402c77
Lance Anderson <lance.a.585@gmail.com>
2023-11-08 19:37:04 -0800

    UPDATE: Simplified logic for spinner

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -9,6 +9,7 @@ import Navbar from "../Navbar";
 export default function NavLayout() {
   // get state from useLocation
   const { state } = useNavigation();
+  const isLoading = state === "loading";
 
   return (
     <>
@@ -17,8 +18,8 @@ export default function NavLayout() {
       {/* ad ScrollRestoration to adjust scroll to top when changing pages */}
       <ScrollRestoration />
       {/* div with "container" arount Outlet for spinner */}
-      <div className={state === "loading" ? "loading-spinner" : ""}></div>
-      <div className={state === "loading" ? "container loading" : "container"}>
+      <div className={isLoading ? "loading-spinner" : ""}></div>
+      <div className={`container ${isLoading ? "loading" : ""}`}>
         <Outlet />
       </div>
     </>
