e88061aa78d1729b9b558fecfa4f4bc59bb31aa8
Lance Anderson <lance.a.585@gmail.com>
2023-11-10 18:03:18 -0800

    ADD: Add comments & post to loader REMOVER: Remove useFetch for comments & posts

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -2,44 +2,46 @@ import { useLoaderData, NavLink, useNavigation } from "react-router-dom";
 import { useFetch } from "../useFetch";
 import { URLS } from "../App";
 import { getPost } from "../api/postsGet";
+import { getComments } from "../api/commentsGet";
+import { getUser } from "../api/usersGet";
 
 export default function Post() {
-  const post = useLoaderData();
+  const { post, comments, user } = useLoaderData();
 
   const { state } = useNavigation();
 
-  const {
-    data: user,
-    isLoading: isUsersLoading,
-    isError: isUsersError,
-  } = useFetch(`${URLS.USERS}/${post.userId}`);
+  // const {
+  //   data: user,
+  //   isLoading: isUsersLoading,
+  //   isError: isUsersError,
+  // } = useFetch(`${URLS.USERS}/${post.userId}`);
 
-  const {
-    data: comments,
-    isLoading: isCommentsLoading,
-    isError: isCommentsError,
-  } = useFetch(`${URLS.POSTS}/${post.id}/comments`);
+  // const {
+  //   data: comments,
+  //   isLoading: isCommentsLoading,
+  //   isError: isCommentsError,
+  // } = useFetch(`${URLS.POSTS}/${post.id}/comments`);
 
-  if (
-    process.env.NODE_ENV === "production" &&
-    (isUsersError || isCommentsError)
-  ) {
-    return <h2>Error fetching data...</h2>;
-  }
+  // if (
+  //   process.env.NODE_ENV === "production" &&
+  //   (isUsersError || isCommentsError)
+  // ) {
+  //   return <h2>Error fetching data...</h2>;
+  // }
 
-  if (
-    process.env.NODE_ENV === "development" &&
-    (isUsersError || isCommentsError)
-  ) {
-    return (
-      <div>
-        <h2>Error fetching data...</h2>
-        <pre>{`${isUsersError && "Users Error: " + isUsersError}\n${
-          isCommentsError && "Comments Error: " + isCommentsError
-        }`}</pre>
-      </div>
-    );
-  }
+  // if (
+  //   process.env.NODE_ENV === "development" &&
+  //   (isUsersError || isCommentsError)
+  // ) {
+  // return (
+  //   <div>
+  //     <h2>Error fetching data...</h2>
+  //     <pre>{`${isUsersError && "Users Error: " + isUsersError}\n${
+  //       isCommentsError && "Comments Error: " + isCommentsError
+  //     }`}</pre>
+  //   </div>
+  // );
+  // }
 
   return (
     <>
@@ -81,10 +83,19 @@ export default function Post() {
   );
 }
 
-function loader({ params, request: { signal } }) {
-  return getPost(params.id, {
+async function loader({ params, request: { signal } }) {
+  const comments = await getComments(params.id, {
     signal,
   });
+  const post = await getPost(params.id, {
+    signal,
+  });
+  const user = await getUser(post.userId, {
+    signal,
+  });
+  console.log("🚀 ~ file: Post.jsx:95 ~ loader ~ post:", post);
+
+  return { comments, post, user };
 }
 
 export const postRoute = {
